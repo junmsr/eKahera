52541b571c4a12f05a500585ec2208da18610d78
dariuuusss <dem2022-7361-81523@bicol-u.edu.ph>
2025-11-14 11:58:13 +0800

    fetch error fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -54,7 +54,7 @@ const app = express();
 const { paymongoWebhook } = require('./controllers/paymentsController');
 app.post('/api/payments/paymongo/webhook', express.raw({ type: '*/*' }), paymongoWebhook);
 
-app.use(cors({ origin: ['https://ekahera.netlify.app', 'http://localhost:5173', 'https://1k4cjk2x-5173.asse.devtunnels.ms', 'https://junmsr.github.io', 'https://www.ekahera.online'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://www.ekahera.online'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
