43abe341b2fe37fae347b7210563390082ecefd4
dariuuusss <dem2022-7361-81523@bicol-u.edu.ph>
2025-11-14 11:04:50 +0800

    Revert "reload nonstop issue3"
    This reverts commit 01457407b0f2b4ecc7f4d9baa802ba0dd0c7f751.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -27,6 +27,7 @@ import InitialSetup from "./pages/InitialSetup.jsx";
 import StoreQR from "./pages/StoreQR.jsx";
 import Receipt from "./pages/Receipt.jsx";
 import Documents from "./pages/Documents.jsx";
+import RedirectHandler from './components/common/RedirectHandler.jsx';
 
 // Router Configuration
 const router = createBrowserRouter(
@@ -58,6 +59,7 @@ const router = createBrowserRouter(
         { path: "superadmin", element: <SuperAdmin /> },
         { path: "superadmin/stores/:id", element: <SuperAdminView /> },
         { path: "setup", element: <InitialSetup /> },
+        { path: "redirect", element: <RedirectHandler /> },
       ],
     },
   ],
